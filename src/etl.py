fd12b47ee2aa5aee439d9c6f9b8a2ebad31f888b
jdtzulg <123261355+jdtzulg@users.noreply.github.com>
2026-01-22 20:17:33 -0600

    Feat: ETL
    ETL implementado

--- src/etl.py
+++ src/etl.py
@@ -1,13 +1,25 @@
 import pandas as pd
 
-def run_etl():
-    """
-    Implementa el proceso ETL.
-    No cambies el nombre de esta función.
-    """
-    # TODO: implementar
-    pass
+ruta = "data/citas_clinica.csv"
 
+def run_etl():
+    df = pd.read_csv(ruta)
+    
+    df = df[df['telefono'].isna()].copy()
+    df['paciente'] = df['paciente'].str.title()
+    df['especialidad'] = df['especialidad'].str.upper()
+
+    df['fecha_cita'] = pd.to_datetime(df['fecha_cita'], errors='coerce')
+    
+    df = df.dropna(subset=['fecha_cita'])
+
+    df = df[df['estado'] == 'CONFIRMADA']
+    df = df[df['costo'] > 0]
+
+    df['telefono'] = df['telefono'].fillna('NO REGISTRA')
+    
+    # Guardar resultado
+    df.to_csv("data/output.csv", index=False)
 
 if __name__ == "__main__":
     run_etl()
